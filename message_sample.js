b8cd74d43db874efcb7b84ee260766220526cf09
col <24493192+coleyon@users.noreply.github.com>
2021-11-17 16:11:39 +0900

    ok

--- message_sample.js
+++
@@ -1,7 +0,0 @@
-const message_sample = Vue.createApp({
-    data: function () {
-        return {
-            message: 'Todo Application message!',
-        }
-    },
-}).mount('#message_sample')
---
+++ src/vue/message_sample.vue
@@ -0,0 +1,2 @@
+const message_sample = Vue.createApp({ data: function () { return { message:
+'Todo Application message!', } }, }).mount('#message_sample')
